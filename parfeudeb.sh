f25c3794a56565a10bd16b506c9212b192d6585c
koye <dkoye23@gmail.com>
2019-10-16 13:06:46 +0100

    Suppression du cache

--- parfeudeb.sh
+++ parfeudeb.sh
@@ -6,4 +6,12 @@ modprobe ip_nat_irc
 modprobe ip_table_nat
 modprobe ip_conntract_ftp
 
+# Suppression des entrees de cache
+iptables -t filter -F
+iptables -t filter -X
+iptables -t filter -F INPUT
+iptables -t filter -F OUTPUT
+iptables -t filter -F FORWARD
+iptables -t nat -F PREROUTING
+
 
